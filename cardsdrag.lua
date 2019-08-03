018e37192f56b9c61182f5bef14698736ee90431
Robert Martin <martinrobert358@gmail.com>
2019-08-03 09:19:40 -0400

    Changes to be committed: 	modified:   cardsdrag.lua
     Changes not staged for commit:
    	modified:   main.lua
    
    Fixed cards clustering together

--- cardsdrag.lua
+++ cardsdrag.lua
@@ -1,14 +1,45 @@
-if mousePressed then
-	
-	local carddrag
+function bool_check()
+	local count = 0
 	for i = 1, 12, 1 do
 		if cardSlots[i][9] then
-			carddrag = cardSlots[i]
-			print(carddrag[1])
+			count = count + 1
 		end
 	end
-	if carddrag then
+	if count == 1 then
+		return true
+	else
+		return false
+	end
+end
+
+
+for i = 1, 12, 1 do
+
+	print(tostring(cardSlots[i][1]) .. "  " .. tostring(cardSlots[i][9]))
+end
+
+
+if mousePressed then
+	local cards = carSlots
+	local carddrag
+	if bool_check() then
+		for i = 1, 12, 1 do
+			if cardSlots[i][9] then
+				dragging = true
+				carddrag = cardSlots[i]
+			end
+		end
+	end
+
+	if not carddrag then
+
+		dragging = false
 			--drag card when the mouse moves
+		mousexA = love.mouse.getX()
+		mouseyA = love.mouse.getY()
+		mousexB = 0
+		mouseyB = 0
+	else
 		if mousexA then
 			mousexB = love.mouse.getX()
 			mouseyB = love.mouse.getY()
@@ -17,15 +48,17 @@ if mousePressed then
 			carddrag[6] = carddrag[6] + mousexB
 			carddrag[7] = carddrag[7] + mouseyB
 		end
-	else
-		mousexA = love.mouse.getX()
-		mouseyA = love.mouse.getY()
-		mousexB = 0
-		mouseyB = 0
 	end
-end--main has cardstouch disabled because it will drag multiple cards at once. I need to think of a better method
+else
+	dragging = false
+end
+if not dragging then
+	for i = 1, 12, 1 do
+		cardSlots[i][9] = cardsTouch(cardSlots[i])
+	end
+end
+--main has cardstouch disabled because it will drag multiple cards at once. I need to think of a better method
 
-		
 	if OS == "Android" then
 		dofile("storage/emulated/0/LOVEGAME/cardstouch.lua")
 	else
