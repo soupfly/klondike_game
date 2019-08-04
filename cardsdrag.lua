8f7abad4f22155edc1ed5497aacfebfe39822f97
Robert Martin <martinrobert358@gmail.com>
2019-08-04 06:22:00 -0400

    modified:   cardsdrag.lua 	modified:   cardstouch.lua 	modified:   main.lua restructure code to clean up the main.lua turned some loops into functions to be called later

--- cardsdrag.lua
+++ cardsdrag.lua
@@ -11,60 +11,44 @@ function bool_check()
 		return false
 	end
 end
-
-
-for i = 1, 12, 1 do
-
-	print(tostring(cardSlots[i][1]) .. "  " .. tostring(cardSlots[i][9]))
-end
-
-
-if mousePressed then
-	local cards = carSlots
-	local carddrag
-	if bool_check() then
-		for i = 1, 12, 1 do
-			if cardSlots[i][9] then
-				dragging = true
-				carddrag = cardSlots[i]
+function cardDrag()
+	if mousePressed then
+		local cards = carSlots
+		local carddrag
+		if bool_check() then
+			for i = 1, 12, 1 do
+				if cardSlots[i][9] then
+					dragging = true
+					carddrag = cardSlots[i]
+				end
 			end
 		end
-	end
-
-	if not carddrag then
-
-		dragging = false
-			--drag card when the mouse moves
-		mousexA = love.mouse.getX()
-		mouseyA = love.mouse.getY()
-		mousexB = 0
-		mouseyB = 0
-	else
-		if mousexA then
-			mousexB = love.mouse.getX()
-			mouseyB = love.mouse.getY()
-			mousexB = mousexB - mousexA
-			mouseyB = mouseyB - mouseyA
-			carddrag[6] = carddrag[6] + mousexB
-			carddrag[7] = carddrag[7] + mouseyB
+		if not carddrag then
+			dragging = false
+			mousexA = love.mouse.getX()
+			mouseyA = love.mouse.getY()
+			mousexB = 0
+			mouseyB = 0
+		else
+			if mousexA then
+				mousexB = love.mouse.getX()
+				mouseyB = love.mouse.getY()
+				mousexB = mousexB - mousexA
+				mouseyB = mouseyB - mouseyA
+				carddrag[6] = carddrag[6] + mousexB
+				carddrag[7] = carddrag[7] + mouseyB
+			end
 		end
-	end
-else
-	dragging = false
-end
-if not dragging then
-	for i = 1, 12, 1 do
-		cardSlots[i][9] = cardsTouch(cardSlots[i])
-	end
-end
---main has cardstouch disabled because it will drag multiple cards at once. I need to think of a better method
-
-	if OS == "Android" then
-		dofile("storage/emulated/0/LOVEGAME/cardstouch.lua")
 	else
-		dofile("card/cardstouch.lua")
+		dragging = false
+	end
+	if not dragging then
+		for i = 1, 12, 1 do
+			cardSlots[i][9] = cardsTouch(cardSlots[i])
+		end
 	end
 	mousexA = love.mouse.getX()
 	mouseyA = love.mouse.getY()
 	mousexB = 0
 	mouseyB = 0
+end
--- main.lua
+++ main.lua
@@ -3,21 +3,15 @@ todo list
 
 make a dummy card. when a card is picked up, the card is put in a variable. slot 14.
 only dummy cards can move when player picks them up. this should solve the problem where players can move multiple cards.
-
-
 ]]--
-
-
 
 function love.load()
 	mousePressed = nil
 	mouseReleased = nil
 	OS = love.system.getOS()
 	DT = nil
-	Ydraw = 0
 	dragging = false
-	timer = 0
---	love.window.setMode(640, 480)
+	love.window.setMode(640, 480)
 	scale = love.graphics.getWidth() / 1920
 	cardSpeed = 600 * scale
 	cardWidth = 237.359 * scale
@@ -36,19 +30,26 @@ function love.load()
 		dofile("storage/emulated/0/LOVEGAME/quads.lua")
 		dofile("storage/emulated/0/LOVEGAME/cardslots.lua")
 		dofile("storage/emulated/0/LOVEGAME/drawfunctions.lua")
+		dofile("storage/emulated/0/LOVEGAME/cardsdrag.lua")
+		dofile("storage/emulated/0/LOVEGAME/cardstouch.lua")
+
 	else
 		dofile("card/cardstouch.lua")
 		dofile("card/quads.lua")
 		dofile("card/cardslots.lua")
 		dofile("card/drawfunctions.lua")
+		dofile("card/cardsdrag.lua")
+		dofile("card/cardstouch.lua")
+
 	end
 end
 
+--I needed a global DT for my draw functions
+--I'll change it later
 function love.update(dt)
-	--I needed a global DT for my draw functions
-	--I'll change it later
 	DT = dt
 	mouseDown = love.mouse.isDown(1)
+	cardDrag()
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -72,40 +73,11 @@ function love.mousereleased(x, y, button)
 end
 
 function love.draw()
-	--loads the cards drag function
-	--this one needs to be reworked
-	--I set it to move a bunch of cards to test it out
-	if OS == "Android" then
-		dofile("storage/emulated/0/LOVEGAME/cardsdrag.lua")
-	else
-		dofile("card/cardsdrag.lua")
-	end
 --draws the playing board
 	love.graphics.setColor(1, 1, 1)
 	love.graphics.draw(images.board, 0, 0, 0, scale, scale)
 --	love.graphics.draw(images.grain, 0, 0, 0, scale, scale)
 	love.graphics.draw(images.tiles, cardQuad.backCard, cardSlots[13][4], cardSlots[13][5], 0, scale, scale)
---[[
-
-First table
-1, 2, 3, 4 --Ace slots
-5 -- Pickslot
-6 -- Stack
-7, 8, 9, 10, 11, 12, 13 -- Row slots
-
-Second Table
-1 -- Slot Name
-2 -- Card Face
-3 -- Card Number
-4 -- X position of row
-5 -- Y position of row
-6 -- X position of card
-7 -- Y position of card
-8 -- Center position of slot on the X axis
-9 -- touch bool
-10 -- drag bool
-]]--
-
 --checks if cards are touching mouse
 	for i = 1, 12, 1 do
 		--cards will zip if they are away from their original spots
