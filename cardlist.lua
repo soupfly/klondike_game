4f8c54416370b644e2e30c10ad96478fdc5570bf
Robert Martin <martinrobert358@gmail.com>
2019-08-07 17:45:36 -0400

    Changes to be committed: 	modified:   cardlist.lua

--- cardlist.lua
+++ cardlist.lua
@@ -23,9 +23,15 @@ cardList = {
 	[3] = {}
 }
 cardlistAdd()
-
+print(#cardList)
+print(#cardList[1])
+print(#cardList[2])
+print(#cardList[3])
 cardList[3] = shuffle(cardList[3])
 --prints the position of the shuffled cards
+--[[
 for i = 1, 52, 1 do
-	print(cardList[3][i][1] .. "  " .. cardList[3][i][2])
+		print(cardList[3][i][1] .. "  " .. cardList[3][i][2])
+
 end
+]]--
